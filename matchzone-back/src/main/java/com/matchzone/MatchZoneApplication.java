6a8b9dbcab601d634da0e6aca26621c9f2a9f931
cristobalcifuentes <c.cifuentes1990@gmail.com>
2025-05-27 20:21:05 -0400

    initial commit

---
+++ matchzone-back/src/main/java/com/matchzone/MatchZoneApplication.java
@@ -0,0 +1,13 @@
+package com.matchzone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchZoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatchZoneApplication.class, args);
+	}
+
+}
